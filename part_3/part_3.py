de940f1346566553e47bb5768fbbc2686f38b2eb
fleurmariepetit <34505146+fleurmariepetit@users.noreply.github.com>
2018-10-08 12:31:28 +0200

    CCG definitions for example sentences

--- part_3/part_3.py
+++ part_3/part_3.py
@@ -1,14 +1,31 @@
 ## part_3
 
+# Example sentences
+
+# 1. im looking for world food
 # S -> S S\S
+# S\S -> (S\S)/PP PP
+# PP -> PP/N N
+# N -> N/N N
+# S -> im
+# (S\S)/PP -> looking
+# PP/N -> for
+# N/N -> world
+# N -> food
+
+# 2. i want a restaurant that serves world food
 # S -> S/NP NP
 # S/NP -> NP NP\(S/NP)
-# S\S -> (S\S)\PP PP
-# S\S -> N N\(S\S)
-# NP -> NP/N N
 # NP -> NP NP\NP
+# NP -> NP/N N
 # NP\NP -> (NP\NP)/N N
 # N -> N/N N
-# N\(S\S) -> (N\(S\S)/N N
-# PP -> PP/N N
-# PP -> PP/NP NP
+# (NP\NP)/N -> ((NP\NP)/N)/(NP\S) NP\S
+# NP -> i
+# NP\(S/NP) -> want
+# NP/N -> a
+# N -> restaurant
+# ((NP\NP)/N)/(NP\S) -> that
+# NP/S -> serves
+# N/N -> world
+# N -> food
