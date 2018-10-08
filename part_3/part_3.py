176b7b56fb29ff48b345a5bb3fdb558aa309e404
fleurmariepetit <34505146+fleurmariepetit@users.noreply.github.com>
2018-10-08 11:45:18 +0200

    CCG definitions for example sentences

--- part_3/part_3.py
+++ part_3/part_3.py
@@ -1,1 +1,13 @@
 ## part_3
+
+# S -> S S\S
+# S -> S/NP NP
+# S/NP -> NP NP\(S/NP)
+# S\S -> (S\S)\PP PP
+# S\S -> N N\(S\S)
+# NP -> NP/N N
+# NP -> NP NP\NP
+# NP\NP -> (NP\NP)/N N
+# N -> N/N N
+# N\(S\S) -> (N\(S\S)/N N
+# PP -> PP N
