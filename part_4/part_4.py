21211d5ff23a1738e5ce351b8237b6097c7bfbe6
SvdWegen <svdwegen@xs4all.nl>
2018-10-20 12:20:52 +0200

    Added speech_acts

--- part_4/part_4.py
+++ part_4/part_4.py
@@ -1,8 +1,101 @@
 import csv
 import random
 
-print('Welcome to the restaurant system. You can ask for restaurants by price, area and the type of food. What would you like?')
+
+"""
+Print welcome
+Recognize speech act of user input
+respond according to speech act
+"""
+
+# read in restaurant data
 restaurant_info = csv.reader(open("restaurantinfo.csv"), delimiter=",")
+
+# train speech act categorization network
+# TODO: train network 
+# Dit is het eerste deel van part_2
+
+# initialize variables
+speech_act = ""
+last_said = ""
+
+# start conversation
+print('Welcome to the restaurant system. You can ask for restaurants by price, area and the type of food. What would you like?')
+
+while speech_act != "bye" and speech_act != "thankyou":
+    # identify speech act
+    #TODO: Find speechact: gebruik user input en getrainde netwerk van part 2
+    #speech_act = 
+    
+    # respond to speech act
+    respond_to_user(speechact)
+    
+    
+def respond_to_user(speech_act):
+    switcher = {
+            "inform": inform,
+            "request": request,
+            "thankyou": thankyou,
+            "null": null,
+            "requalts": requalts,
+            "affirm": affirm,
+            "bye": bye,
+            "ack": ack,
+            "hello": hello,
+            "negate": negate,
+            "repeat": repeat,
+            "reqmore": reqmore, 
+            "restart": restart,
+            "confirm": confirm,
+            "deny": deny
+            }
+
+    func = switcher.get(speech_act, "speechact unknown")
+    return func()
+
+#TODO: Definieer elke speechact functie
+def inform():
+    
+def request():
+    
+def thankyou():
+    print("You're welcome. Goodbye!")
+    exit()
+    
+def null():
+    last_said = "Sorry, I don't understand what you mean"
+    print(last_said)
+    
+def reqalts():
+
+def affirm():
+    
+def bye():
+    exit()
+    
+def ack():
+    last_said = "Is there anything else you want to know?"
+    print(last_said)
+    
+def hello():
+    last_said = "Hello, what type of food, price range and area are you looking for?"
+    print(last_said)
+    
+def negate():
+
+def repeat():
+    print(last_said)
+    
+def reqmore():
+    
+def restart():
+    
+def confirm():
+    
+def deny():
+    
+
+
 food = input('What type of food would you like?')
 price = input('What is your price range?')
 area = input('What part of town?')
@@ -10,7 +103,7 @@ restaurants = []
 phones = []
 adresses = []
 postcodes = []
-speech_act = ""
+
 
 for row in restaurant_info:
     #if all of the criteria are provided
